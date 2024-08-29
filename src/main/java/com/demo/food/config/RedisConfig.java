215eb002730ec0edb0e9952b6593517fe81c3145
Myhgan <hang08hb@gmail.com>
2024-08-29 13:30:43 +0700

    seting and using redis for category

---
+++ src/main/java/com/demo/food/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.demo.food.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnection;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnection(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");
+        configuration.setPort(6380);
+//        configuration.setDatabase(0);
+//        configuration.setUsername();
+//        configuration.setPassword();
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnection){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnection);
+
+        return template;
+
+    }
+}
--- src/main/java/com/demo/food/service/CategoryService.java
+++ src/main/java/com/demo/food/service/CategoryService.java
@@ -6,12 +6,16 @@ import com.demo.food.entity.Category;
 import com.demo.food.entity.Food;
 import com.demo.food.repository.CategoryRepository;
 import com.demo.food.service.imp.CategoryServiceImp;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,27 +23,40 @@ import java.util.List;
 public class CategoryService implements CategoryServiceImp {
     @Autowired
     CategoryRepository categoryRepository;
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    private Gson gson = new Gson();
     @Override
     public List<CategoryDTO> getCategoryHomePage() {
         PageRequest pageRequest = PageRequest.of(0, 2, Sort.by("id"));
-        Page<Category> listCategory = categoryRepository.findAll(pageRequest);
+        String dataRedis = (String) redisTemplate.opsForValue().get("category");
         List<CategoryDTO> listCategoryDTOS = new ArrayList<>();
+        if(dataRedis == null){
+            Page<Category> listCategory = categoryRepository.findAll(pageRequest);
 
-        for (Category data : listCategory) {
-            CategoryDTO categoryDTO = new CategoryDTO();
-            categoryDTO.setName(data.getNameCate());
+            for (Category data : listCategory) {
+                CategoryDTO categoryDTO = new CategoryDTO();
+                categoryDTO.setName(data.getNameCate());
 
-            List<MenuDTO> menuDTOS = new ArrayList<>();
-            for (Food dataFood : data.getListFood()) {
-                MenuDTO menuDTO = new MenuDTO();
-                menuDTO.setTitle((dataFood.getTitle()));
-                menuDTO.setFreeship(dataFood.getFreeship());
-                menuDTO.setImage(dataFood.getImage());
-                menuDTOS.add(menuDTO);
+                List<MenuDTO> menuDTOS = new ArrayList<>();
+                for (Food dataFood : data.getListFood()) {
+                    MenuDTO menuDTO = new MenuDTO();
+                    menuDTO.setTitle((dataFood.getTitle()));
+                    menuDTO.setFreeship(dataFood.getFreeship());
+                    menuDTO.setImage(dataFood.getImage());
+                    menuDTOS.add(menuDTO);
 
+                }
+                categoryDTO.setMenus(menuDTOS);
+                listCategoryDTOS.add(categoryDTO);
             }
-            categoryDTO.setMenus(menuDTOS);
-            listCategoryDTOS.add(categoryDTO);
+            String dataJson = gson.toJson(listCategoryDTOS);
+            redisTemplate.opsForValue().set("category", dataJson);
+
+        }else {
+            Type listType = new TypeToken<List<CategoryDTO>>(){}.getType();
+            listCategoryDTOS = gson.fromJson(dataRedis, listType);
         }
 
         return listCategoryDTOS;
